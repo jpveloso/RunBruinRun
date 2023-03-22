2096644d725d9d17f1565e4e0de883d34e3ba82c
velosoj <velosoj@sheridancollege.ca>
2023-03-22 15:03:38 -0400

    test merge

---
+++ run_bruin_run/build/app/generated/source/buildConfig/debug/sheridan/ASP/run_bruin_run/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package sheridan.ASP.run_bruin_run;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "sheridan.ASP.run_bruin_run";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
