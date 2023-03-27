11729df07509c18f23d4cda4504120d36897583f
alvin <alvintian2003@yahoo.ca>
2023-03-27 10:33:25 -0400

    testing firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:run_bruin_run/screens/homepage/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
